d085fb0893559db33a056b9be78a7e6e204906af
siva100625 <sivasubramanian.v2023ece@sece.ac.ingamil.com>
2025-10-22 15:25:44 +0530

    second

--- worm-detector/src/api/apiClient.js
+++ worm-detector/src/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api', // Your Django API URL
+    baseURL: 'https://iotbackend-h5go.onrender.com/api', // Your Django API URL
 });
 
 export default apiClient;
